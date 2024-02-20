12915d283a67cde764d48605dcb2b619834d2719
Carlos Villegas <2030072@upt.edu.mx>
2024-02-20 00:43:11 -0600

    Load More Data component ready

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -17,7 +18,13 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image Slider component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={"3"} limit={'9'}/>
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"3"} limit={'9'}/> */}
+
+
+      {/* Loading more data component */}
+      <LoadMoreData/>
+
+
     </div>
   );
 }
