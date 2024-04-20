5a3c18985e89a55ae3c01cbe819b3c2eb0eb987d
Carlos Villegas <2030072@upt.edu.mx>
2024-04-20 16:55:13 -0600

    Custom tabs init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
-import TreeView from './components/tree-view';
-import menus from './components/tree-view/data';
-import QRCodeGenerator from './components/qr-code-generator';
-import LightDarkMode from './components/light-dark-mode';
+// import Accordian from './components/accordian';
+// import RandomColor from './components/random-color';
+// import StarRating from './components/star-rating';
+// import ImageSlider from './components/image-slider';
+// import LoadMoreData from './components/load-more-data';
+// import TreeView from './components/tree-view';
+// import menus from './components/tree-view/data';
+// import QRCodeGenerator from './components/qr-code-generator';
+// import LightDarkMode from './components/light-dark-mode';
+// import ScrollIndicator from './components/scroll-indicator';
+// import Tabs from './components/custom-tabs/tabs';
+import TabTest from './components/custom-tabs/tab-test';
 
 function App() {
   return (
@@ -35,7 +38,14 @@ function App() {
       {/* <QRCodeGenerator/> */}
 
       {/* Light and Dark mode component */}
-      <LightDarkMode/>
+      {/* <LightDarkMode/> */}
+      {/* Scroll indicator component */}
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/> */}
+
+      {/* Custom tabs component */}
+
+      {/* <TabTest/> */}
+      <TabTest/>
 
 
     </div>
